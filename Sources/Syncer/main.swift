4ff5e1d47d67ab753c3d32c58c51be52d40f77f4
Joe Smith <yasumoto7@gmail.com>
2019-03-08 10:41:01 -0800

    Control destination directory and more logging

--- Sources/Syncer/main.swift
+++ Sources/Syncer/main.swift
@@ -1,28 +1,34 @@
 import Dispatch
 import Foundation
 
+
+func outputLog(_ contents: String) {
+    let now = Date()
+    let format = DateFormatter()
+    format.dateFormat = "MM-dd-yyyy HH:mm:ss"
+    print("[\(format.string(from: now))] \(contents)")
+}
+
 var args = CommandLine.arguments
 
 if (args.contains("--help") || args.contains("-h")) {
-    print("Syncer <destination_host> <path>")
+    print("Syncer <destination_host> <remote_path>")
     print("Built from https://github.com/Yasumoto/Syncer")
     exit(1)
 }
 
-guard args.count == 3, let path = args.popLast(), let hostname = args.popLast() else {
-    print("Please specify a target host and file's path to watch")
+guard args.count == 3, let remotePath = args.popLast(), let hostname = args.popLast() else {
+    print("Please specify your target host and the destination path you want to sync to")
     exit(1)
 }
 
-let format = DateFormatter()
-format.dateFormat = "MM-dd-yyyy HH:mm:ss"
+let currentWorkingDirectory = FileManager.default.currentDirectoryPath
 
 let trigger = DispatchWorkItem {
-    let start = Date()
-    print("[\(format.string(from: start))] Starting sync")
+    outputLog("Starting sync to \(hostname)")
     let task = Process()
     task.launchPath = "/usr/bin/rsync"
-    task.arguments = ["-a", path, "\(hostname):"]
+    task.arguments = ["-a", currentWorkingDirectory, "\(hostname):\(remotePath)"]
     let stdout = Pipe()
     let stderr = Pipe()
     task.standardOutput = stdout
@@ -30,14 +36,17 @@ let trigger = DispatchWorkItem {
     task.standardError = stderr
     task.launch()
     task.waitUntilExit()
-    let done = Date()
-    print("[\(format.string(from: done))] Finished syncing \(path)")
+    if task.terminationStatus != 0,
+        let output = String(data: stderr.fileHandleForReading.availableData, encoding: .utf8) {
+        outputLog("Error syncing!\n\(output)")
+    }
+    outputLog("Finished syncing \(currentWorkingDirectory) to \(remotePath)")
 }
 
-guard let watcher = try? Watcher(path: path, trigger: trigger) else {
-    print("Could not watch: \(path)")
+guard let watcher = try? Watcher(path: currentWorkingDirectory, trigger: trigger) else {
+    outputLog("Could not watch: \(currentWorkingDirectory)")
     exit(1)
 }
 
-print("Watching \(path)")
+outputLog("Watching \(currentWorkingDirectory)")
 RunLoop.current.run()
